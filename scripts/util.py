feafada98e2f1a61566aefa6d448eb4f6b218c16
Greg V <greg.vannoni@gmail.com>
2023-10-04 09:25:42 -0700

    Updates to util

--- scripts/util.py
+++ scripts/util.py
@@ -12,7 +12,7 @@ logger.addHandler(logging.StreamHandler())
 # set log level
 logger.setLevel(logging.INFO)
 #
-from common.utils.slack import send_slack
+from common.utils.slack import send_slack, get_active_users
 from common.utils.firebase import get_hackathon_by_event_id, get_nonprofit_by_id, add_image_to_nonprofit_by_nonprofit_id, add_image_to_nonprofit, add_nonprofit_to_hackathon, create_new_problem_statement, create_new_nonprofit, create_new_hackathon, link_nonprofit_to_problem_statement, link_problem_statement_to_hackathon_event, get_nonprofit_by_name, create_team, add_user_by_email_to_team, add_user_by_slack_id_to_team, add_team_to_hackathon, add_problem_statement_to_team, get_user_by_user_id, add_reference_link_to_problem_statement, get_user_by_email, create_user, add_user_to_team, delete_user_by_id, get_team_by_name, get_user_by_id, remove_user_from_team
 from common.utils.cdn import upload_to_cdn
 import re
@@ -76,20 +76,23 @@ def util_add_image_to_nonprofit(nonprofit_id=None, nonprofit_name=None, image_ur
 
 
 
-
-    
-
-
-  
 # Handle command-line arguments
 parser = argparse.ArgumentParser(description='Utility script for database actions')
 parser.add_argument('action', type=str, help='action to perform')
 parser.add_argument('--nonprofit_name', type=str, help='nonprofit name')
 parser.add_argument('--nonprofit_id', type=str, help='nonprofit id')
+parser.add_argument('--description', type=str, help='nonprofit description')
+parser.add_argument('--website', type=str, help='nonprofit website')
+parser.add_argument('--contact_name', type=str, help='nonprofit contact name')
+
 parser.add_argument('--hackathon_event_id', type=str, help='hackathon event id')
 parser.add_argument('--problem_statement_id', type=str, help='problem statement id')
 parser.add_argument('--image_url', type=str, help='image url')
 parser.add_argument('--team_id', type=str, help='team id')
+
+parser.add_argument('--slack_channel', type=str, help='user slack channel')
+parser.add_argument('--slack_message', type=str, help='slack message')
+
 args = parser.parse_args()
 
 # Perform action
@@ -99,6 +102,17 @@ elif args.action == "add_problem_statement_to_team":
     add_problem_statement_to_team(args.problem_statement_id, args.team_id)
 elif args.action == "add_image_to_nonprofit":
     util_add_image_to_nonprofit(nonprofit_name=args.nonprofit_name, nonprofit_id=args.nonprofit_id, image_url=args.image_url)
+elif args.action == "send_slack":
+    send_slack(channel=args.slack_channel, message=args.slack_message)
+elif args.action == "active_slack_users":
+    print("\n".join(get_active_users()))
+elif args.action == "create_new_nonprofit":
+    create_new_nonprofit(
+        name=args.nonprofit_name, description=args.description, 
+        website=args.website, 
+        slack_channel=args.slack_channel,
+        contact_people=args.contact_name,
+        image=args.image_url)
 
 else:
     # Print help
