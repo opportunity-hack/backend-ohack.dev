dbb292ca58f2fd7b7d33b9acdd88efd90cd18c74
Squibb <71113694+tmspe859@users.noreply.github.com>
2023-11-06 05:45:05 +0000

    Changed hooks to Post

--- api/certificates/certificate_views.py
+++ api/certificates/certificate_views.py
@@ -1,3 +1,5 @@
+from typing import Dict
+
 from flask import Blueprint
 from flask import request
 from api.certificates.certificate_service import generate_certificate, validateCertificate
@@ -6,16 +8,16 @@ bp_name = "api-certificates"
 bp_url_prefix = "/api/certificates"
 bp = Blueprint(bp_name, __name__, url_prefix=bp_url_prefix)
 
-@bp.route("/generate/<string:username>/<path:repoUrl>", methods=["POST"])
-def generateCertificate(repoUrl: str, username: str):
+@bp.route("/generate/", methods=["POST"])
+def generateCertificate() -> Dict[str, str]:
     form = request.form
     if ("repoURL" not in form or "username" not in form): return {}
     repoUrl: str = form["repoURL"]
     username: str = form["username"]
     return {"img data": generate_certificate(repoUrl, username)}
 
-@bp.route("/verify/<string:username>/<path:repoUrl>", methods=["POST"])
-def verifyCertificate():
+@bp.route("/verify/", methods=["POST"])
+def verifyCertificate() -> Dict[str, str]:
     form = request.form
     if ("image" not in form): return {}
     imgData = form["image"]
